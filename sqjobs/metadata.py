65a74590b4d5d3fd32b3fc245336893086e4e813
Carlos Edo <carlosedo14@gmail.com>
2019-07-18 09:10:43 +0200

    dont set retry time before execution

--- sqjobs/metadata.py
+++ sqjobs/metadata.py
@@ -1,6 +1,6 @@
 __uname__ = 'sqjobstf'
 __long_name__ = 'Simple Queue Jobs TF Fork'
-__version__ = '0.8.1'
+__version__ = '0.8.2'
 __author__ = 'Saul Diez-Guerra'
 __email__ = 'saul@thinkful.com'
 __url__ = 'http://github.com/Thinkful/sqjobs'
