1a1893712e42fbc6a47b6e6a00b030b31beee9d1
Javi <javier.olivares@thinkful.com>
2021-04-01 20:09:27 +0100

    Restore package name, increase version

--- sqjobs/metadata.py
+++ sqjobs/metadata.py
@@ -1,6 +1,6 @@
-__uname__ = 'sqjobs'
+__uname__ = 'sqjobstf'
 __long_name__ = 'Simple Queue Jobs TF Fork'
-__version__ = '0.9.0'
+__version__ = '0.8.6'
 __author__ = 'Saul Diez-Guerra'
 __email__ = 'saul@thinkful.com'
 __url__ = 'http://github.com/Thinkful/sqjobs'
