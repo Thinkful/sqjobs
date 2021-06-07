1fd2e049f4d103bbd581100bb4d5c4191f220731
Javi <javier.olivares@thinkful.com>
2021-06-07 22:28:07 +0100

    Rename env var

--- sqjobs/connectors/sqs.py
+++ sqjobs/connectors/sqs.py
@@ -152,7 +152,7 @@ class SQS(Connector):
     def _get_queue(self, name):
         try:
             environment = os.environ.get('ENVIRONMENT', False)
-            aws_queues_enabled = int(os.environ.get('AWS_SAM_QUEUES', 0))
+            aws_queues_enabled = int(os.environ.get('SQS_SAM_ENABLE', 0))
             if environment and aws_queues_enabled:
                 # running in the AWS environments
                 name = f'{environment}-{name}'
